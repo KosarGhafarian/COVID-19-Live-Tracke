16adc13586cfac0fc0b5f7acc1b53121ade1766f
Kothar-2000 <kotharghafarian2000@gmail.com>
2024-08-09 18:42:14 +0330

    Update App.js

--- covid19/src/App.js
+++ covid19/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import "./assets/styles/app.css";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
